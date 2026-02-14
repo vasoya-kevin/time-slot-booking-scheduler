0ca1b14e76a2c3eaa1396f1bb92c8aad1983be37
Kevin <kevin.vasoya@outlook.com>
2026-02-14 23:26:49 +0530

    update the controllers

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,13 +3,11 @@ import { Router } from "express";
 import authRouter from "./auth.routes.ts";
 import availabilityRouter from "./availability.routes.ts";
 import bookingLinksRouter from "./booking-link.routes.ts";
-import bookingRouter from "./bookings.routes.ts";
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/availability', availabilityRouter);
 router.use('/booking-links', bookingLinksRouter);
-router.use('/bookings', bookingRouter);
 
 export default router
