ae343fc5eba8511b7d8d72903f4cf9d9130d8cd8
Kevin <kevin.vasoya@outlook.com>
2026-02-14 12:21:09 +0530

    intial push

---
+++ server/src/middlewares/middlewares.ts
@@ -0,0 +1,7 @@
+// middlewares.ts
+import { RequestHandler } from "express";
+
+export const middleware: RequestHandler = (req, res) => {
+    res.send("Hello World!");
+    console.log("Response sent");
+};
